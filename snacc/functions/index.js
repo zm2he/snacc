47feef719106929a7c3fd0852b84633fa6089d67
bruce-he-62 <bruce.he.62@gmail.com>
2019-09-27 00:28:13 -0400

    Remove deadcode

--- snacc/functions/index.js
+++ snacc/functions/index.js
@@ -51,37 +51,5 @@ exports.onInventoryAdd = functions.firestore.document('inventory/{inventoryId}')
         .catch(function(err) {
             console.log(err);
         });
-
-/*
-        return https.get('https://api.edamam.com/api/food-database/parser?app_id=19649da1&app_key=636ca13a6d644daec03cd524e0fb5a61&ingr=' + change.data().name,
-            (resp) => {
-                resp.on('end', () => {
-                    console.log(d.text);
-                })
-            }
-        )
-        .on("error", (err) => {
-            console.log("Error: " + err);
-        });
-*/
-        /*
-        return request.get('https://api.edamam.com/api/food-database/parser?app_id=19649da1&app_key=636ca13a6d644daec03cd524e0fb5a61&ingr=' + change.data().name,
-            (error, response, body) => {
-                let food = response.parsed[0].food;
-                console.log(food);
-                console.log("slkadjfkdsa");
-                return inventoryRef.doc(inventoryId).update({
-                    "foodId": food.foodId,
-                    "label": food.label,
-                    "calories": food.nutrients.ENERC_KCAL,
-                    "protein": food.nutrients.PROCNT, 
-                    "carbs": food.nutrients.CHOCDF,
-                    "fat": food.nutrients.FAT,
-                    "fiber": food.nutrients.FIBTG,
-                })
-                .catch(console.log(error));
-            }
-        );
-        */
     }
 );
